cf35dda8b72ef7d8f5331b06d0868878894a78dc
Ricardo_T8 <ricardoryn1317@gmail.com>
2025-03-10 15:33:59 +0800

    update

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .brain_plot import plot_human_brain_figure, plot_human_hemi_brain_figure, plot_macaque_brain_figure, plot_macaque_hemi_brain_figure
-from .common_plot import plot_one_group_bar_figure, plot_one_group_violin_figure, plot_correlation_figure, plot_matrix_figure
-from .circos_plot import plot_symmetric_circle_figure, plot_asymmetric_circle_figure
+from .brain_plot import *
+from .common_plot import *
+from .circos_plot import *
